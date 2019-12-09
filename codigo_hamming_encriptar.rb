b82e5d8939595b6b435b47a42cfa0a9885835030
Natanael Silva <natanso.s17@hotmail.com>
2019-12-09 13:04:02 -0300

    incompleto

--- codigo_hamming_encriptar.rb
+++ codigo_hamming_encriptar.rb
@@ -10,23 +10,9 @@ caracteres_irregulares = mensagem.select { |caractere| caractere != '1' && carac
 
 if caracteres_irregulares.nil? || caracteres_irregulares.empty?
   # parte da criacao dos bits de paridade
-  contador = 0
   bits_de_verificacao = {}
   bits_de_verificacao_valor = {}
-  loop do
-    indice_paridade = 2**contador
-
-    if  indice_paridade <= mensagem.length
-      # insere o bit de paridade em seu local devido
-      mensagem.insert(indice_paridade - 1, "v#{indice_paridade}")
-      # insere no array de bits de paridades
-      bits_de_verificacao["v#{indice_paridade}"] = []
-      bits_de_verificacao_valor["v#{indice_paridade}"] = []
-      contador += 1
-    else
-      break
-    end
-  end
+  inicializar_arrays_de_posicoes(mensagem, bits_de_verificacao, bits_de_verificacao_valor)
 
   # declarar o array que vai ter as posicoes dos bit de dados
   posicao_bits_de_dados = []
@@ -37,6 +23,7 @@ if caracteres_irregulares.nil? || caracteres_irregulares.empty?
     posicao_bits_de_dados.append(index + 1) unless bit.include?('v')
   end
 
+  # 
   bits_de_dados_para_cada_de_verificacao(mensagem, posicao_bits_de_dados, posicao_bits_de_verificacao, bits_de_verificacao,  bits_de_verificacao_valor)
 
   valor_bits_verificacao(mensagem, bits_de_verificacao_valor, paridade)
--- funcoes.rb
+++ funcoes.rb
@@ -1,3 +1,21 @@
+def inicializar_arrays_de_posicoes(mensagem, bits_de_verificacao, bits_de_verificacao_valor)
+  contador = 0
+  loop do
+    indice_paridade = 2**contador
+
+    if  indice_paridade <= mensagem.length
+      # insere o bit de paridade em seu local devido
+      mensagem.insert(indice_paridade - 1, "v#{indice_paridade}")
+      # insere no array de bits de paridades
+      bits_de_verificacao["v#{indice_paridade}"] = []
+      bits_de_verificacao_valor["v#{indice_paridade}"] = []
+      contador += 1
+    else
+      break
+    end
+  end
+end
+
 def  bits_de_dados_para_cada_de_verificacao(mensagem, posicao_bits_de_dados, posicao_bits_de_verificacao, bits_de_verificacao,  bits_de_verificacao_valor)
   posicao_bits_de_dados.each do |bit_dados_posicao|
     bit_dados_posicao_aux = bit_dados_posicao
@@ -18,7 +36,6 @@ def valor_bits_verificacao(mensagem, bits_de_verificacao_valor, paridade)
   bits_de_verificacao_valor.keys.each do |key|
     if paridade == 'par'
       soma = bits_de_verificacao_valor[key].sum
-      p soma
       if soma.even?
         mensagem[mensagem.index(key.to_s)] = '0'
       else
